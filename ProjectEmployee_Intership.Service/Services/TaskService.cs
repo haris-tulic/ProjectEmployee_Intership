4e58498fc70ad25dacce78581f1ff7eb7f817117
HarisTulic <HarisTulic@JAP-J3SVXT2>
2022-06-08 17:03:50 +0200

    AddTaskInterface

---
+++ ProjectEmployee_Intership.Service/Services/TaskService.cs
@@ -0,0 +1,48 @@
+﻿using Intership_ProjectTeam4.Database;
+using ProjectEmployee_Intership.Core.Models.Dto;
+using ProjectEmployee_Intership.Core.Models.Request;
+using ProjectEmployee_Intership.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEmployee_Intership.Service.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly ProjectUserContext _context;
+
+        public TaskService(ProjectUserContext context)
+        {
+            _context = context;
+        }
+
+
+        public Task<TasksDto> AddNewTask(AddTaskRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TasksDto> DeleteTask(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TasksDto>> GetAllTasks(GetTaskRequest search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TasksDto> GetTaskById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TasksDto> UpdateTask(AddTaskRequest request, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
