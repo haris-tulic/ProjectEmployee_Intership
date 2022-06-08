a74c0ec6d6f1974311acfbaac9eb9ec67db305b7
HarisTulic <HarisTulic@JAP-J3SVXT2>
2022-06-08 15:20:28 +0200

    CreatingNewDatabase

--- ProjectEmployee_Intership.Core/Entities/Project.cs
+++ ProjectEmployee_Intership.Core/Entities/Project.cs
@@ -1,5 +1,4 @@
-﻿using Intership_ProjectTeam4.Enum;
-using ProjectEmployee_Intership.Common.Enums;
+﻿using ProjectEmployee_Intership.Common.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace Intership_ProjectTeam4.Database
@@ -13,7 +12,7 @@ namespace Intership_ProjectTeam4.Database
         public DateTime FinishDate { get; set; }
         public StatusProject Status { get; set; }
         public bool IsDeleted { get; set; } = false;
-        public List<ProjectEmployee> Employees { get; set; }
+        public List<ProjectUser> ProjectUsers { get; set; }
         public List<Tasks> Tasks { get; set; }
         
     }
