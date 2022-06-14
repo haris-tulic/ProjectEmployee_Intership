e924d60c5208881ed7529872c5caf191402c4c2d
HarisTulic <HarisTulic@JAP-J3SVXT2>
2022-06-14 16:04:07 +0200

    Changes

---
+++ ProjectEmployee_Intership.Database/Migrations/20220614124001_ChangesInUserTable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectEmployee_Intership.Database.Migrations
+{
+    public partial class ChangesInUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Projects_ProjectId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Projects_ProjectId",
+                table: "Users",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Projects_ProjectId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Projects_ProjectId",
+                table: "Users",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
